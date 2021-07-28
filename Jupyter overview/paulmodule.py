a08e9ffad6418346eb21039b6633577710acff07
Nishimirwe <nishimirwepaul20105@gmail.com>
2021-07-28 17:50:40 +0200

    Working on modules and learning other tricks

--- Jupyter overview/paulmodule.py
+++ Jupyter overview/paulmodule.py
@@ -16,3 +16,29 @@ def power(base,exp):
         return 1
     else:
         return base*power(base,exp-1)
+    
+    
+# Method to check odd or even number
+def isEven(n):
+    return n%2==0
+if __name__=="__main__":
+    print("I am open")
+if __name__!="__main__":
+    print()
+    print()
+    print("paulmodule is imported")
+    print()
+    print()
+
+class Node:
+    number=-1;
+    def __init__(self,v):
+        print("Node is declared")
+        self.data=v
+        Node.number+=1
+        self.index=Node.number
+        self.next=None
+        
+    def show(self):
+        print(self.data)
+ 
---
+++ Jupyter overview/project1.py
@@ -0,0 +1,27 @@
+import paulmodule
+
+f=paulmodule.fib(8)
+print(f"fib({8})={f}")
+print("Imana irakarama")
+for i in range(10):
+    print("I love you")
+
+li=[i for i in range(1,50)]
+arr=filter(paulmodule.isEven,li)
+arr=list(arr)
+print(len(arr))
+
+print(__name__)
+
+# I am going to use Node class from paul module
+lON=[]
+node=paulmodule.Node(45)
+lON.append(node)
+node1=paulmodule.Node(70)
+lON.append(node1)
+node.show()
+print(f"index is {node.index}")
+
+print(f"second node is {node1.data}, and its index is {node1.index}, now having the total of {paulmodule.Node.number+1} nodes")
+for i in lON:
+    print(i.data," ",end=''),
