4d627c6b942c001ec5c94589f569a373e82ef771
Nishimirwe <nishimirwepaul20105@gmail.com>
2021-07-30 18:02:54 +0200

    Entering real data science

--- paulmodule/allinone.py
+++ paulmodule/allinone.py
@@ -26,3 +26,193 @@
             # isPrime(number), return True if number is prime number, otherwise False
             # isPalindrome(string), return True if a string is palindrome, otherwise false
             # isAnagram(string1,string2), return True if string1 is anagram of string2 (string1 is formed by rearranging string2)
+       
+    # Node for singly linked List
+class Node:
+    def __init__(self, value):
+        self.data=value
+        self.next=None
+        
+# A linked List which is using above Node class
+
+class SLList:
+    
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        self.length=0
+
+    def size(self):
+        return self.length
+
+    def isEmpty(self):
+        return self.head==None
+
+    def append(self, value):
+        node=Node(value)
+        if(self.isEmpty()):
+            self.head=self.tail=node
+            self.length+=1
+        else:
+            self.tail.next=node
+            self.tail=node
+            self.length+=1
+
+    def show(self):
+        if self.isEmpty():
+            print(f"List is empty")
+        else:
+            nav=self.head
+            while nav:
+                print(f"{nav.data} ",end='')
+                nav=nav.next
+            print()
+            print(f"size: {self.length}")
+    
+    def toList(self):
+        if self.isEmpty():
+            return False,"List is empty"
+        else:
+            li=list()
+            nav=self.head
+            while nav:
+                li.append(nav.data)
+                nav=nav.next
+            return li
+        
+    def addFirst(self,value):
+        node=Node(value)
+        node.next=self.head
+        self.head=node
+        self.length+=1
+
+    def insert(self,value,pos):
+        if pos<0 or pos>self.length:
+            print("Invalid position")
+        elif pos==0:
+            self.addFirst(value)
+        elif pos==self.length:
+            self.append(value)
+        else:
+            node=Node(value)
+            i=1;
+            nav=self.head
+            while i<pos:
+                nav=nav.next
+                i+=1
+            node.next=nav.next
+            nav.next=node
+            self.length+=1
+
+    def clear(self):
+        self.head=None
+        self.tail=None
+        self.length=0
+        
+    def sort(self):
+        li=self.toList()
+        li.sort()
+        self.clear()
+        for i in li:
+            self.append(i)
+
+    def sortAsc(self):
+        li=self.toList()
+        li.sort()
+        self.clear()
+        for i in li:
+            self.append(i)
+            
+            
+    def sortDesc(self):
+        li=self.toList()
+        li.sort(reverse=True)
+        self.clear()
+        for i in li:
+            self.append(i)
+            
+    def reverse(self):
+        li=self.toList()
+        li.reverse()
+        self.clear()
+        for i in li:
+            self.append(i)
+            
+    def deleteFirst(self):
+        if self.isEmpty():
+            print("List head in None")
+        else:
+            x=self.head.data
+            self.head=self.head.next
+            self.length-=1
+            return x
+                  
+    def pop(self):
+        if self.isEmpty():
+            return -1
+        else:
+            nav=self.head
+            t=None
+            while nav!=self.tail:
+                t=nav  
+                nav=nav.next
+            t.next=None
+            self.tail=t
+            self.length-=1
+            return nav.data
+
+    def deleteAt(self,pos):
+        if pos<0 or pos>self.length-1:
+            return -1
+        elif pos==0:
+            self.deleteFirst()
+        elif pos==self.length-1:
+            self.pop()
+        else:
+            i=1
+            nav=self.head
+            while i<pos:
+                nav=nav.next
+                i+=1
+            t=nav.next
+            x=t.data
+            nav.next=t.next
+            self.length-=1
+            return x
+            
+                  
+                
+                
+# Function to take a python list or tupple and turn it to SLList
+def toSLList(li):
+    if len(li)==0:
+        print("Cannot take empty list")
+    else:
+        sllist=SLList()
+        for i in li:
+            sllist.append(i)
+        return sllist
+    
+
+# Returning the fibonacci serie number on any order
+def fib(n):
+    if n==0 or n==1:
+        return n
+    else:
+        return fib(n-1)+fib(n-2)
+    
+    
+#Returning the factorial of a number
+def fact(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n*fact(n-1)
+    
+#Calculate the gcd of a number
+def gcd(a,b):
+    if b==0:
+        return a
+    return gcd(b,a%b)
+
+
---
+++ paulmodule/module_test_in_terminal.py
@@ -0,0 +1,4 @@
+import allinone as aio
+
+print(aio.fact(5))
+print(f"GCD of {20} and {10} = {aio.gcd(20,18)}")
